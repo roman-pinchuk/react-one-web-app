f8274fc1944601bf43f02e410c0c4e2840af3bfc
Roman Pinchuk <pinchuk.roman@outlook.com>
2019-05-22 22:01:30 +0300

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,48 +19,58 @@ import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function RenderComments({ comments, postComment, dishId }) {
-  if (comments) {
-    const commentsList = comments.map(comment => {
-      return (
-        <li key={comment.id}>
-          <p>{comment.comment}</p>
-          <p>
-            -- {comment.author} &nbsp;
-            {new Intl.DateTimeFormat("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "2-digit"
-            }).format(new Date(comment.date))}
-          </p>
-        </li>
-      );
-    });
-    return (
-      <div>
-        <h4> Comments </h4>
-        <ul className="list-unstyled">
-          {commentsList}
-          <CommentForm dishId={dishId} postComment={postComment} />
-        </ul>
-      </div>
+  if (comments != null) {
+    let commentList = (
+      <Stagger in>
+        {comments.map(comment => {
+          return (
+            <Fade in key={comment.id}>
+              <li>
+                <p>{comment.comment}</p>
+                <p>
+                  -- {comment.author} ,{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit"
+                  }).format(new Date(Date.parse(comment.date)))}
+                </p>
+              </li>
+            </Fade>
+          );
+        })}
+      </Stagger>
     );
-  } else {
-    return <div />;
-  }
+    return (
+      <React.Fragment>
+        <h3>Comments</h3>
+        <ul className="list-unstyled">{commentList}</ul>
+        <CommentForm dishId={dishId} postComment={postComment} />
+      </React.Fragment>
+    );
+  } else return <div />;
 }
 
 function RenderDish({ dish }) {
   if (dish) {
     return (
-      <Card>
-        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+      <FadeTransform
+        in
+        transformProps={{
+          exitTransform: "scale(0.5) translateY(-50%)"
+        }}
+      >
+        <Card>
+          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
     );
   } else {
     return <div />;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,6 +9,7 @@ import {
   CardSubtitle
 } from "reactstrap";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform } from "react-animation-components";
 
 function RenderCard({ item, isLoading, errMess }) {
   if (isLoading) {
@@ -17,17 +18,23 @@ function RenderCard({ item, isLoading, errMess }) {
     return <h4>{errMess}</h4>;
   } else if (item != null) {
     return (
-      <Card>
-        <CardImg src={baseUrl + item.image} alt={item.name} />
-        <CardBody>
-          <CardTitle>{item.name}</CardTitle>
-          {/* If item.designation is not null do fist, if not do second. */}
-          {item.designation ? (
-            <CardSubtitle>{item.designation}</CardSubtitle>
-          ) : null}
-          <CardText>{item.description}</CardText>
-        </CardBody>
-      </Card>
+      <FadeTransform
+        in
+        transformProps={{
+          exitTransform: "scale(0.5) translateY(-50%)"
+        }}
+      >
+        <Card>
+          <CardImg src={baseUrl + item.image} alt={item.name} />
+          <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? (
+              <CardSubtitle>{item.designation}</CardSubtitle>
+            ) : null}
+            <CardText>{item.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
     );
   } else {
     return <div />;
